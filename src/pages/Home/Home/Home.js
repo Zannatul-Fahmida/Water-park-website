124b74e85600f2a77a40a1ada3715bb956b3a650
Zannatul-Fahmida <jannatulfahmida15@gmail.com>
2022-01-01 21:53:00 +0600

    first commit

---
+++ src/pages/Home/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Client from '../Client/Client';
+
+const Home = () => {
+    return (
+        <div>
+            <Client />
+        </div>
+    );
+};
+
+export default Home;
