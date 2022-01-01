a0984fa2a24170a6159750e5f3a4b0dddb8015bc
chandan-mandi <itscoderchandan@gmail.com>
2022-01-02 02:43:15 +0530

    I created review and gallery section

--- src/pages/Home/Home/Home.js
+++ src/pages/Home/Home/Home.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import Client from '../Client/Client';
+import ImgGallery from '../Gallery/ImgGallery';
+import Reviews from '../Reviews/Reviews';
 
 const Home = () => {
     return (
         <div>
             <Client />
+            <Reviews />
+            <ImgGallery />
         </div>
     );
 };
