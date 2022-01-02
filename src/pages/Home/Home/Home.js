f068d29859558522f2fcdea93ad606ab2f4a96d7
MK-Raihan <mkraihan207@gmail.com>
2022-01-02 22:16:13 +0600

    nav banner subcriber

--- src/pages/Home/Home/Home.js
+++ src/pages/Home/Home/Home.js
@@ -1,15 +1,19 @@
 import React from 'react';
+import Banner from '../../Banner/Banner';
 import Blog from '../Blogs/Blogs';
 import Footer from '../Footer/Footer';
 import HappyClient from '../HappyClient/HappyClient';
 import Holiday from '../Holiday/Holiday';
+import Subscriber from '../Subscriber/Subscriber';
 
 const Home = () => {
     return (
         <div>
+            <Banner/>
             <Holiday />
             <HappyClient />
             <Blog />
+            <Subscriber/>
             <Footer />
         </div>
     );
