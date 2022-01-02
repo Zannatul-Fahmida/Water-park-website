56ed8f5e5f5f324bdebac4a99a9dfc6d9d4c0d2f
Zannatul-Fahmida <jannatulfahmida15@gmail.com>
2022-01-02 13:26:33 +0600

    animation added

--- src/pages/Home/HappyClient/HappyClient.js
+++ src/pages/Home/HappyClient/HappyClient.js
@@ -4,30 +4,37 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 import { Col, Container, Row } from 'react-bootstrap';
 import './HappyClient.css';
+import CountUp from 'react-countup';
 
 const HappyClient = () => {
     return (
         <Container className="bg-info client-container text-white">
             <Row className="py-5">
                 <Col xs={6} md={3}>
-                <h1><FontAwesomeIcon icon={faUsers} /></h1>
-                <h1><span className="number">8,450</span> +</h1>
-                <p className="fw-bold">Happy Visitors</p>
+                    <h1><FontAwesomeIcon icon={faUsers} /></h1>
+                    <h1><span className="number"><CountUp
+                        separator=","
+                        start={1000}
+                        end={8450}
+                        duration={2}
+                        delay={0}
+                    /></span> +</h1>
+                    <p className="fw-bold">Happy Visitors</p>
                 </Col>
                 <Col xs={6} md={3}>
-                <h1><FontAwesomeIcon icon={faWater} /></h1>
-                <h1><span className="number">30</span> +</h1>
-                <p className="fw-bold">World Class Rides</p>
+                    <h1><FontAwesomeIcon icon={faWater} /></h1>
+                    <h1><span className="number"><CountUp start={0} end={30} duration={2} delay={0} /></span> +</h1>
+                    <p className="fw-bold">World Class Rides</p>
                 </Col>
                 <Col xs={6} md={3}>
-                <h1><FontAwesomeIcon icon={faThumbsUp} /></h1>
-                <h1><span className="number">15</span> +</h1>
-                <p className="fw-bold">Years Of Experience</p>
+                    <h1><FontAwesomeIcon icon={faThumbsUp} /></h1>
+                    <h1><span className="number"><CountUp start={0} end={15} duration={2} delay={0} /></span> +</h1>
+                    <p className="fw-bold">Years Of Experience</p>
                 </Col>
                 <Col xs={6} md={3}>
-                <h1><FontAwesomeIcon icon={faUserNurse} /></h1>
-                <h1><span className="number">40</span> +</h1>
-                <p className="fw-bold">Certified Lifeguard</p>
+                    <h1><FontAwesomeIcon icon={faUserNurse} /></h1>
+                    <h1><span className="number"><CountUp start={0} end={40} duration={2} delay={0} /></span> +</h1>
+                    <p className="fw-bold">Certified Lifeguard</p>
                 </Col>
             </Row>
         </Container>
